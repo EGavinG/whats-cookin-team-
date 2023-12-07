df9c6dc3384f44be79f3ce7753469559daad9e13
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-12-07 14:25:50 -0800

    Refactor display functionality

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -9,12 +9,40 @@ import {
 
 import recipeData from "./data/recipes";
 import ingredientsData from "./data/ingredients";
+// Query Selectors
+const resultsContainer = document.querySelector(".results-container");
+const recipePage = document.getElementById("recipe-page");
+const searchInput = document.querySelector(".search-bar input");
+const submitButton = document.getElementById("submitButton");
+const tagsContainer = document.querySelector('.tags');
 
+// Event listeners
+
+submitButton.addEventListener("click", function (){
+  search();
+});
+
+tagsContainer.addEventListener('click', function(event){
+  displayFilterByTag(event)
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  setupSubmitButtonListener();
+});
+
+// document
+//   .querySelector(".search-bar form")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault(); // Prevent the default form submission behavior
+
+//     const searchInput = document.getElementById("searchInput").value;
+//     const filteredRecipes = filterRecipesByName(recipeData, searchInput);
+//     updateResultsContainer(filteredRecipes);
+//   });
 // DOM manipulation functions
-function showAllRecipes(recipes) {
-  const resultsContainer = document.querySelector(".results-container");
-  const recipePage = document.getElementById("recipe-page");
 
+function showAllRecipes(recipes) {
   recipes.forEach((recipe, index) => {
     const recipeCard = createRecipeCard(recipe);
     resultsContainer.appendChild(recipeCard);
@@ -22,7 +50,7 @@ function showAllRecipes(recipes) {
 }
 
 function showRecipePage(recipe) {
-  const recipePage = document.getElementById("recipe-page");
+  
   recipePage.innerHTML = `
     <h1>${recipe.title}</h1>
     <img src="${recipe.image}" alt="${recipe.title}">
@@ -30,7 +58,7 @@ function showRecipePage(recipe) {
 }
 
 function updateFilteredResults(recipes) {
-  const searchInput = document.querySelector(".search-bar input");
+ 
   const tags = Array.from(document.querySelectorAll(".tags a"))
     .filter((tag) => tag.classList.contains("selected"))
     .map((tag) => tag.innerText);
@@ -39,7 +67,7 @@ function updateFilteredResults(recipes) {
 }
 
 function updateResultsContainer(recipes) {
-  const resultsContainer = document.querySelector(".results-container");
+  
   resultsContainer.innerHTML = "";
 
   recipes.forEach((recipe) => {
@@ -68,36 +96,23 @@ function createRecipeCard(recipe) {
   return recipeCard;
 }
 
-// Event listeners
-document
-  .querySelector(".search-bar form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-
-    const searchInput = document.getElementById("searchInput").value;
-    const filteredRecipes = filterRecipesByName(recipeData, searchInput);
-    updateResultsContainer(filteredRecipes);
-  });
-
-document.querySelectorAll(".tags a").forEach((tag) => {
-  tag.addEventListener("click", () => {
-    tag.classList.toggle("selected");
-    updateFilteredResults(recipeData);
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  setupSubmitButtonListener();
-});
-
-function setupSubmitButtonListener() {
-  const submitButton = document.getElementById("submitButton");
-  submitButton.addEventListener("click", search);
+function displayFilterByTag(event) {
+  const clickedTag = event.target.closest('.tag-btn')
+  console.log(clickedTag.id);
+  if(clickedTag) {
+    const tag = clickedTag.id
+    console.log(tag);
+  const filteredRecipesByTag = filterRecipesByTag(recipeData, tag)
+  console.log(filteredRecipesByTag);
+  updateResultsContainer(filteredRecipesByTag);
+  goBackToMain();
+  // showAllRecipes(filterRecipesByTag)
+  }
 }
 
 function search() {
-  const searchInput = document.getElementById("searchInput").value;
-  const filteredRecipes = filterRecipesByName(recipeData, searchInput);
+  const searchInputValue = document.getElementById("searchInput").value;
+  const filteredRecipes = filterRecipesByName(recipeData, searchInputValue);
   updateResultsContainer(filteredRecipes);
   goBackToMain();
 }
